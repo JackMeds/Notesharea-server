5dd8ca4a037a121145cbdd4f68ce5c1304b4ccfc
Anthonysakura <2380249269@qq.com>
2023-10-16 16:39:18 +0800

    完善了个人中心

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,18 +2,19 @@
  * @description user controller
  */
 
-const { getUserInfo, createUser } = require('../services/user');
+const { getUserInfo, createUser, updateUser, getPeronaldataInfo } = require('../services/user');
 const { SuccessModel, ErrorModel } = require('../model/ResModel');
 const { registerUserNameNotExistInfo, registerUserNameExistInfo, registerFailInfo, loginFailInfo } = require('../model/ErrorInfo');
 const doCrypto = require('../utils/cryp');
+
 
 /**
  * 
  * @param {string} userName 用户名
  */
-async function isExist({userName}) {
+async function isExist({ userName }) {
     // console.log("controller"+userName);
-    const userInfo = await getUserInfo({userName});
+    const userInfo = await getUserInfo({ userName });
     // console.log(userInfo);
     if (userInfo) {
         //已存在
@@ -33,7 +34,7 @@ async function isExist({userName}) {
  * @param {number} gender 性别 (1 男， 2 女， 3 保密) 默认 3
  */
 async function register({ userName, password, gender, nickName, email, phoneNum, userIntro, picture }) {
-    const userInfo = await getUserInfo({userName});
+    const userInfo = await getUserInfo({ userName });
     // console.log(userInfo);
     if (userInfo) {
         //用户名已存在
@@ -70,12 +71,12 @@ async function login(ctx, userName, password) {
     //登陆成功 ctx.session.userInfo = xxx
     //获取用户信息
     const loginMessage = {
-        userName, 
+        userName,
         password: doCrypto(password)
     }
     console.log(loginMessage);
     const userInfo = await getUserInfo(loginMessage);
-    if (!userInfo){
+    if (!userInfo) {
         //登陆失败
         // console.log(userInfo);
         return new ErrorModel(loginFailInfo);
@@ -92,8 +93,85 @@ async function login(ctx, userName, password) {
     return new SuccessModel(data);
 }
 
+// 获取个人信息数据
+async function getPeronalInfo(ctx) {
+    const userId = ctx.query.userId;
+    console.log('control',userId);
+    try {
+      const userInfo = await getPeronaldataInfo(userId);
+      return userInfo.dataValues; // 返回数据
+    //   ctx.body = userInfo.dataValues;
+    //   console.log('controls',ctx.body);
+    } catch (error) {
+      console.error('Error in getUserInfoController:', error);
+      ctx.status = 500;
+      ctx.body = { error: 'Internal Server Error' };
+    }
+  }
+
+
+/**
+ * @param {object} ctx ctx
+ * @param {string} userName 用户名
+ * @param {string} nickName 昵称
+ * @param {string} email 邮箱
+ * @param {string} phoneNum 手机号码
+ * @param {string} userIntro 个人简介
+ * @param {string} picture 头像
+ */
+async function changeInfo(ctx, {userId, nickName,gender, email, phoneNum, userIntro, picture }) {
+    try {
+        //  userId = ctx.session.userInfo.id;
+        if (!userId) {
+            throw new Error('User ID is not valid');
+        }
+
+        // 构造更新数据
+        const updateData = {
+            newNickName: nickName,
+            newGender:gender,
+            newEmail: email,
+            newPhoneNum: phoneNum,
+            newUserIntro: userIntro,
+            newPicture: picture,
+        };
+
+        console.log('control', updateData);
+        console.log('controlid',userId);
+
+        // 调用更新函数
+        const result = await updateUser(updateData,userId);
+        console.log('controlss',result);
+        if (result && result.code !== undefined) {
+            // 执行成功
+            
+            Object.assign(ctx.session.userInfo, {
+                nickName,
+                gender,
+                email,
+                phoneNum,
+                userIntro,
+                picture,
+            });
+            // 返回成功模型
+            return new SuccessModel(message,'success');
+        } else {
+            // 失败
+            return new ErrorModel({ errno: 10009, message: 'Change info failed' });
+        }
+    } catch (error) {
+        console.error('Error in changeInfo:', error);
+        return new ErrorModel('Internal Server Error');
+    }
+}
+
+
+
+
 module.exports = {
     isExist,
     register,
-    login
+    login,
+    changeInfo,
+    getPeronalInfo,
 }
